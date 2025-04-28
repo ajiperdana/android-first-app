d3070833a80560bf17de57dadf7bf75a6a13524a
ajiperdana <58857860+ajiperdana@users.noreply.github.com>
2025-04-28 23:47:57 +0800

    1st commit

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,11 +40,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Cyan) {
-        Text(
-            text = "Hi, my name is $name!",
-            modifier = modifier.padding(24.dp)
-        )
+    Box(contentAlignment = Alignment.Center, modifier = modifier.padding(24.dp)){
+
+        Surface (
+            color = Color.LightGray,
+            modifier = modifier
+                .padding(24.dp)
+                .shadow(10.dp)
+
+        ){
+            Text(
+                text = "Hi, my name is $name!",
+                modifier = modifier.padding(24.dp)
+            )
+        }
+
     }
 }
 
